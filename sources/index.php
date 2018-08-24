0941d81c0c0829019641c4d6259cdf4a5c769be4
Lucas Pereira <vulcanun@hotmail.com>
2018-08-24 16:33:24 -0300

    Update index.php

--- sources/index.php
+++ sources/index.php
@@ -1,6 +1,6 @@
 <?php
-
 	$data= $_SERVER['REQUEST_METHOD'] . ";" . $_SERVER['REMOTE_ADDR'] . ";";
-	file_put_contents('../logs/accessTry.log', $data, "FILE_APPEND");
 
+	if(!is_dir("../logs/")) mkdir($dir);
+	file_put_contents('../logs/accessTry.log', $data, "FILE_APPEND");
 ?>
